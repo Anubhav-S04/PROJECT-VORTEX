af0e67ddd814f40e8f1f6a49c22cc901797e61ec
Anubhav Saha <anubhavsaha142@gmail.com>
2025-08-17 01:37:23 +0530

    mobile view navbar dropdown menu close icon added

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-  const trigger = document.getElementById('triggerBox');
-  const dropdown = document.getElementById('dropdownBox');
+const trigger = document.getElementById('triggerBox');
+const dropdown = document.getElementById('dropdownBox');
 
-  trigger.addEventListener('click', () => {
-    dropdown.classList.toggle('hidden');
-  });
+trigger.addEventListener('click', () => {
+  dropdown.classList.toggle('hidden');
+  trigger.classList.toggle('active'); // toggles hamburger ↔ X
+});
 
 
-    document.addEventListener('click', (e) => {
-    if (!document.getElementById('dropdownContainer').contains(e.target)) {
-      dropdown.classList.add('hidden');
-    }
-  });
+document.addEventListener('click', (e) => {
+  if (!document.getElementById('dropdownContainer').contains(e.target)) {
+    dropdown.classList.add('hidden');
+    trigger.classList.remove('active'); // reset back to hamburger
+  }
+});
